f62af08732cf78bcb48ae0c73c82f099062cc844
nakashi94 <n0904yBUNJOU@gmail.com>
2021-12-31 13:27:40 +0900

    Initial Commit

---
+++ Janken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Janken
+//
+//  Created by Nakashima Yuta on 2021/12/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Janken/JankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenApp.swift
+//  Janken
+//
+//  Created by Nakashima Yuta on 2021/12/31.
+//
+
+import SwiftUI
+
+@main
+struct JankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
